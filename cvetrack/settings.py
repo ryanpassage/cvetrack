d26e569d34da0011bca13cde4023ef0cc2ba66ff
Ryan Passage <ryan.passage@lexmark.com>
2022-02-03 13:06:36 -0500

    Create Django apps

--- cvetrack/settings.py
+++ cvetrack/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pond^ez2q%+kww--oxm$#)hu@5+xlz93bv@3yev0p28nhpsz)k'
+SECRET_KEY = '#@9$9j2ksi(#)1$(@3*$fvnbo6c6$8-^e_6(92#dvs$!oz734p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'api'
 ]
 
 MIDDLEWARE = [
