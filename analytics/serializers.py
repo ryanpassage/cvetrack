6b74de7939a94c68174ca04e1987011236eafaa3
Ryan Passage <ryan.passage@lexmark.com>
2022-02-21 15:21:47 -0500

    Delete analytics module

--- analytics/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from analytics.models import Device
-
-class DeviceSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Device
-        fields = ('serial_number', 'last_seen', 'firmware_major', 'firmware_minor', 'firmware_build', 'vulnerable_cves',)
